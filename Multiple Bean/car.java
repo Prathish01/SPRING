c53db6e5dcf2226a13103f294dafb9cd0c1d3365
Prathish S <prathish472@gmail.com>
2026-05-26 20:28:43 +0530

    Update car.java

--- Multiple Bean/car.java
+++ Multiple Bean/car.java
@@ -2,17 +2,24 @@ package com.hibernate.bean;
 
 import lombok.Data;
 
+
 @Data
 public class car 
+  
 {
+  
 private int carid;
 private String brand;
 private String model;
 private String colour;
 private String price;
 
+  
 public car()
+  
 {
+  
 System.out.println("object created");	
+  
 }
 }
