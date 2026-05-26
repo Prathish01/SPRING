388069feab672f10c27507c2226e8b2b0603527a
Prathish S <prathish472@gmail.com>
2026-05-26 20:34:01 +0530

    Update mainclass.java

--- PROJECT 1/mainclass.java
+++ PROJECT 1/mainclass.java
@@ -10,17 +10,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example")
 @EnableJpaRepositories(basePackages = "com.example.repository")
 @EntityScan(basePackages = "com.example.entity")
 public class UserappApplication {
 
+
     
 
     public static void main(String[] args) {
         SpringApplication.run(UserappApplication.class, args);
     }
+
+    
 
     @Bean
     public ModelMapper modelMapper() {
