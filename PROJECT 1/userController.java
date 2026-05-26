7ad5765f8138abe7c917e99fc6b50fd0f7520bd6
Prathish S <prathish472@gmail.com>
2026-05-26 20:35:04 +0530

    Update userController.java

--- PROJECT 1/userController.java
+++ PROJECT 1/userController.java
@@ -4,17 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.entity.User;
+
 import com.example.service.UserService;
 import com.example.userdto.Userdto;
 import com.example.util.ResponseStructure;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
