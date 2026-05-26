e0c7866fd7f68373a4fca9ae520407442a4ca7db
Prathish S <prathish472@gmail.com>
2026-05-26 20:31:01 +0530

    Update mainclass.java

--- AUTOWIRING (ByType)/mainclass.java
+++ AUTOWIRING (ByType)/mainclass.java
@@ -3,13 +3,19 @@ package com.spring.bytpe;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 import com.spring.bytpe.*;
 import com.spring.dao.car;
 /**
+
  * Hello world!
  */
-public class App {
-    public static void main(String[] args) {
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        
         ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/bean.xml");
         
         car c1 = context.getBean("car1", car.class);
