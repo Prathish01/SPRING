b3dfe2c4c389d3937fa9a1c2aa2bdbf95f509dd0
Prathish S <prathish472@gmail.com>
2026-05-26 20:34:17 +0530

    Update responsestructure.java

--- PROJECT 1/responsestructure.java
+++ PROJECT 1/responsestructure.java
@@ -2,7 +2,11 @@ package com.example.util;
 import java.time.LocalDateTime;
 import lombok.Data;
 @Data
+
+
+	
 public class ResponseStructure<T>
+	
 {
 	private T  data;
 	private LocalDateTime timeStamp;
