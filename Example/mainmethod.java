bf45b26c0fc5bf6b8b3f0b54941d3c8978499953
Prathish S <prathish472@gmail.com>
2026-05-31 17:04:34 +0530

    Update mainmethod.java

--- Example/mainmethod.java
+++ Example/mainmethod.java
@@ -7,15 +7,18 @@ import com.jspiders.dao.student;
 
 /**
  * Hello world!
+ 
  */
 public class App {
     public static void main(String[] args) {
 
+        
 
     	ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/bean.xml");
     	student stu = context.getBean("student1",student.class);
     	student stu1 = context.getBean("student1",student.class);
-    	
+
+        
     	System.out.println(stu);
     	System.out.println(stu1);
     }
