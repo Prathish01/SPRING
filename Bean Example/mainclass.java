3e0dab65a7d78f2b57d0551f8dd01eff3bb72dab
Prathish S <prathish472@gmail.com>
2026-05-31 17:06:40 +0530

    Update mainclass.java

--- Bean Example/mainclass.java
+++ Bean Example/mainclass.java
@@ -9,7 +9,10 @@ import com.hibernate.dao.employee;
 
 
 public class App {
+
+    
     public static void main(String[] args) {
+        
         ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/bean.xml");
         
         employee bean = context.getBean("emp1",employee.class);
@@ -27,6 +30,8 @@ public class App {
         System.out.println(bean4);
         
         employee bean5 = context.getBean("emp5",employee.class);
+        
         System.out.println(bean5);
+        
     }
 }
