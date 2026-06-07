3bd9c37bff87b23a231e49b5a7b3e3bd11895515
Prathish S <prathish472@gmail.com>
2026-06-07 21:45:37 +0530

    Update car.java

--- AUTOWIRING/car.java
+++ AUTOWIRING/car.java
@@ -1,10 +1,12 @@
 package com.spring.dao;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
 
 @Getter
 @Setter
