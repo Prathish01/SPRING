840dc279bfdd0a028f0fe665d17193641627d3c0
Prathish S <prathish472@gmail.com>
2026-06-12 17:45:59 +0530

    Update student.java

--- Example/student.java
+++ Example/student.java
@@ -1,21 +1,13 @@
 package com.jspiders.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import lombok.Data;
-
 @Data
 public class student 
 {
-  
 private int Studentid;
 private String name;
 private String gender;
 @Autowired
 private school scho;
-  
 }
-
-
-
-
