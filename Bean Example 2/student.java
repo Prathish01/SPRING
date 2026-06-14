2b556012db7e9c094f1720da46262c7cc7fd33cb
Prathish S <prathish472@gmail.com>
2026-06-14 17:02:55 +0530

    Update student.java

--- Bean Example 2/student.java
+++ Bean Example 2/student.java
@@ -5,17 +5,12 @@ import lombok.Data;
 @Data
 public class student 
 {
-
-	
 	private int studentId;
     private String studentName;
     private String course;
     private String department;
     private String age;
     private String gender;
-	
-    
-    
     public student()
     {
 		
